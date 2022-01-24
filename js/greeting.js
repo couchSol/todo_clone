3d3b0c806c94a7574f1c9cfe587de3864c0f5d36
couchSol <96866819+couchSol@users.noreply.github.com>
2022-01-24 21:34:51 +0900

    Add files via upload

---
+++ js/greeting.js
@@ -0,0 +1,30 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+// 로그인 과정
+function onLoginSubmit(event){
+    event.preventDefault();
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    paintGreetings(username);
+}
+
+function paintGreetings(putUsername){
+    greeting.innerText = `Hello ${putUsername}!`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+// 로그인 여부 확인
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+}else{
+    paintGreetings(savedUsername);
+}
